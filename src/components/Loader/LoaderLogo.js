6eb164d873379cc9e3672e353dff5edbe48506d8
Aditya Siddharth Jyoti <155255348+arch-adi21@users.noreply.github.com>
2024-05-01 16:06:24 +0530

    Update LoaderLogo.js

--- src/components/Loader/LoaderLogo.js
+++ src/components/Loader/LoaderLogo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const LogoLoader = (props) => {
   return (
     <div className="loader-container">
-      <img src={require('/vercel/path0/src/components/LoaderLoader.svg')} alt="Loader" />
+      <img src={require('/vercel/path0/src/components/Loader/Loader.svg')} alt="Loader" />
     </div>
   );
 };
